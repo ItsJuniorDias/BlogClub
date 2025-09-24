5df85f47f3049cfd8edb3a19bb34bb52499babd5
Alexandre Junior <alexandrejunior@192.168.1.3>
2025-09-24 11:25:57 -0300

    chore: adjust color bottom tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,38 +1,57 @@
-// app/_layout.tsx
-import { Ionicons } from "@expo/vector-icons";
-import { BlurView } from "expo-blur";
 import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
-import { StyleSheet } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import TabBar from "../(new-article)/components/tab-bar/tab-bar";
-import ButtonFloat from "../(profile)/components/buttonFloat";
 import { Colors } from "../../constants/Colors";
-import { useRouter } from "expo-router";
 
 export default function Layout() {
-  const insets = useSafeAreaInsets();
-  const router = useRouter();
-
   return (
     <NativeTabs
       backgroundColor="transparent"
-      rippleColor={Colors.light.darkblueInfo}
-      indicatorColor={Colors.light.darkblueInfo}
+      rippleColor={Colors.light.darkBlue}
+      indicatorColor={Colors.light.darkBlue}
     >
       <NativeTabs.Trigger name="home">
-        <Label>Home</Label>
-        <Icon sf={"house.fill"} drawable="ic_menu_home" />
+        <Label
+          selectedStyle={{
+            color: Colors.light.darkblueInfo,
+          }}
+        >
+          Home
+        </Label>
+        <Icon
+          sf={"house.fill"}
+          drawable="ic_menu_home"
+          selectedColor={Colors.light.darkblueInfo}
+        />
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="new-article">
-        <Label>New Article</Label>
-        <Icon sf={"book.fill"} drawable="ic_menu_manage" />
+        <Label
+          selectedStyle={{
+            color: Colors.light.darkblueInfo,
+          }}
+        >
+          New Article
+        </Label>
+        <Icon
+          sf={"book.fill"}
+          drawable="ic_menu_manage"
+          selectedColor={Colors.light.darkblueInfo}
+        />
         {/* <Badge>9+</Badge> */}
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="profile">
-        <Label>Profile</Label>
-        <Icon sf={"person.fill"} drawable="ic_menu_manage" />
+        <Label
+          selectedStyle={{
+            color: Colors.light.darkblueInfo,
+          }}
+        >
+          Profile
+        </Label>
+        <Icon
+          sf={"person.fill"}
+          drawable="ic_menu_manage"
+          selectedColor={Colors.light.darkblueInfo}
+        />
         {/* <Badge>9+</Badge> */}
       </NativeTabs.Trigger>
     </NativeTabs>
