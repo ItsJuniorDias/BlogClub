b194fe70e60872c9559616b501f2317f09321b08
ItsJuniorDias <reabilitado97@gmail.com>
2025-10-02 15:50:14 -0300

    chore: animation splash screen

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,34 +1,51 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useRef } from "react";
+import { Animated, Easing } from "react-native";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-import logo from "../../assets/images/logo.png";
-
+import logo from "../../assets/images/icon.png";
 import { useRouter } from "expo-router";
 import { useUserStore } from "../../store/useUserStore";
 import { queryUserByUID } from "../../utils/queryUserByUID";
-import { Container, Logo } from "./styles";
+import { Container } from "./styles";
 
 export default function SplashScreen() {
   const { fetch } = useUserStore();
-
   const router = useRouter();
-
   const auth = getAuth();
+
+  const translateX = useRef(new Animated.Value(300)).current;
+  const scale = useRef(new Animated.Value(1)).current;
+  const opacity = useRef(new Animated.Value(1)).current;
 
   const handleAuthState = async () => {
     onAuthStateChanged(auth, async (user) => {
       const uid = user?.uid;
-
       fetch(queryUserByUID(uid));
 
-      if (user) {
-        return router.push("/(tabs)/home");
-      } else {
-        setTimeout(() => {
-          return router.push("/(onboarding)");
-        }, 2000);
-      }
+      Animated.sequence([
+        Animated.timing(translateX, {
+          toValue: 0,
+          duration: 1000,
+          easing: Easing.out(Easing.exp),
+          useNativeDriver: true,
+        }),
+        Animated.timing(scale, {
+          toValue: 1.5,
+          duration: 600,
+          easing: Easing.out(Easing.circle),
+          useNativeDriver: true,
+        }),
+        Animated.timing(opacity, {
+          toValue: 0,
+          duration: 800,
+          useNativeDriver: true,
+        }),
+      ]).start(() => {
+        if (user) {
+          router.push("/(tabs)/home");
+        } else {
+          router.push("/(onboarding)");
+        }
+      });
     });
   };
 
@@ -38,7 +55,16 @@ export default function SplashScreen() {
 
   return (
     <Container>
-      <Logo source={logo} />
+      <Animated.Image
+        source={logo}
+        style={{
+          width: 120,
+          height: 120,
+          transform: [{ translateX }, { scale }],
+          opacity,
+        }}
+        resizeMode="contain"
+      />
     </Container>
   );
 }
--- app/(app)/styles.ts
+++ app/(app)/styles.ts
@@ -1,14 +1,15 @@
+import { Colors } from "@/constants/Colors";
 import { Image } from "expo-image";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #e5eaf2;
+  background-color: ${Colors.light.background};
   align-items: center;
   justify-content: center;
 `;
 
 export const Logo = styled(Image)`
   width: 161px;
-  height: 81px;
+  height: 161px;
 `;
