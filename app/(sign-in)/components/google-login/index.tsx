71fda80635c575af18f6a656a6307e1600b16440
ItsJuniorDias <reabilitado97@gmail.com>
2025-10-04 10:32:27 -0300

    chore: style destructive button

--- app/(sign-in)/components/google-login/index.tsx
+++ app/(sign-in)/components/google-login/index.tsx
@@ -47,7 +47,7 @@ export default function GoogleLogin() {
         [
           {
             text: "Cancel",
-            style: "cancel",
+            style: "destructive",
           },
           {
             text: "OK",
