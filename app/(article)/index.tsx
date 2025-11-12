d9652b66dca8e94e388b391f8374a284d466b88e
ItsJuniorDias <reabilitado97@gmail.com>
2025-11-12 13:15:32 -0300

    chore: adjust dele article

--- app/(article)/index.tsx
+++ app/(article)/index.tsx
@@ -247,19 +247,26 @@ export default function ArticleScreen() {
 
   const renderContextMenu = () => {
     return (
-      <Host style={{ width: 150, height: 50 }}>
-        <ContextMenu>
-          <ContextMenu.Items>
-            <Button systemImage="xmark" onPress={() => handleDelete(data.id)}>
-              Delete
-            </Button>
-          </ContextMenu.Items>
+      <>
+        {currentUser?.uid === data.foreign_key && (
+          <Host style={{ width: 150, height: 50 }}>
+            <ContextMenu>
+              <ContextMenu.Items>
+                <Button
+                  systemImage="xmark"
+                  onPress={() => handleDelete(data.id)}
+                >
+                  Delete
+                </Button>
+              </ContextMenu.Items>
 
-          <ContextMenu.Trigger>
-            <Button variant="bordered">Show Menu</Button>
-          </ContextMenu.Trigger>
-        </ContextMenu>
-      </Host>
+              <ContextMenu.Trigger>
+                <Button variant="bordered">Show Menu</Button>
+              </ContextMenu.Trigger>
+            </ContextMenu>
+          </Host>
+        )}
+      </>
     );
   };
 
